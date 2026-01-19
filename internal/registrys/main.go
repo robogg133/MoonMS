94e17b480d9c73122818d769ab09932e7aedfe11
robogg133 <Luizfernandojoaquimdasilva@proton.me>
2026-01-19 18:42:56 -0300

    added registry serialize and all registry methods

---
+++ internal/registrys/main.go
@@ -0,0 +1,42 @@
+package registrys
+
+import (
+	"MoonMS/internal/datatypes"
+	"MoonMS/internal/packets"
+	"bytes"
+)
+
+type RegistryData struct {
+	DimensionIdentifier string
+	NBTData             []byte
+}
+
+type Registry struct {
+	AllData []RegistryData
+}
+
+func (reg *Registry) Serialize() []byte {
+	var response bytes.Buffer
+
+	var allDataBuffer bytes.Buffer
+
+	var prefixedLength int32 = 0
+
+	for _, data := range reg.AllData {
+		allDataBuffer.Write(datatypes.NewString(data.DimensionIdentifier))
+		allDataBuffer.Write(data.NBTData)
+		prefixedLength++
+	}
+
+	packetId := datatypes.NewVarInt(packets.PACKET_REGISTRY_DATA)
+
+	prefixedLenghtSerialized := datatypes.NewVarInt(prefixedLength)
+
+	response.Write(datatypes.NewVarInt(int32(len(packetId) + len(prefixedLenghtSerialized) + allDataBuffer.Len())))
+
+	response.Write(packetId)
+	response.Write(prefixedLenghtSerialized)
+	response.Write(allDataBuffer.Bytes())
+
+	return response.Bytes()
+}
