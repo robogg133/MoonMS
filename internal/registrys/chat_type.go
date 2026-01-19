b999249d97175f9cfb6ad1a17c7a6a77fc77a1e7
robogg133 <Luizfernandojoaquimdasilva@proton.me>
2026-01-19 18:43:21 -0300

    added chat registry type

---
+++ internal/registrys/chat_type.go
@@ -0,0 +1,17 @@
+package registrys
+
+const REGISTRY_CHAT string = "minecraft:chat_type"
+
+const (
+	CHAT_CHAT = "minecraft:chat"
+
+	CHAT_EMOTE_COMMAND = "minecraft:emote_command"
+
+	CHAT_COMMAND_INCOMING = "minecraft:command_incoming"
+	CHAT_COMMAND_OUTGOING = "minecraft:command_outgoing"
+
+	CHAT_SAY_COMMAND = "minecraft:say_command"
+
+	CHAT_TEAM_MSG_COMMAND_INCOMING = "minecraft:team_msg_command_incoming"
+	CHAT_TEAM_MSG_COMMAND_OUTGOING = "minecraft:team_msg_command_outgoing"
+)
