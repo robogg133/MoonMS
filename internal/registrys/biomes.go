946a208c75f3cfd08b67f4cfd5ea01f3b1896142
robogg133 <Luizfernandojoaquimdasilva@proton.me>
2026-01-19 18:43:44 -0300

    added chat registry type

---
+++ internal/registrys/biomes.go
@@ -0,0 +1,104 @@
+package registrys
+
+const REGISTRY_BIOME string = "minecraft:worldgen/biome"
+
+// Overworld biomes
+const (
+	// Plains
+	BIOME_PLAINS           = "minecraft:plains"
+	BIOME_MEADOW           = "minecraft:meadow"
+	BIOME_SUNFLOWER_PLAINS = "minecraft:sunflower_plains"
+
+	// Deserts and badlands
+	BIOME_DESERT          = "minecraft:desert"
+	BIOME_BADLANDS        = "minecraft:badlands"
+	BIOME_ERODED_BADLANDS = "minecraft:eroded_badlands"
+	BIOME_WOODED_BADLANDS = "minecraft:wooded_badlands"
+
+	// Savanna
+	BIOME_SAVANNA           = "minecraft:savanna"
+	BIOME_SAVANNA_PLATEAU   = "minecraft:savanna_plateau"
+	BIOME_WINDSWEPT_SAVANNA = "minecraft:windswept_savanna"
+
+	// Forest
+	BIOME_FOREST        = "minecraft:forest"
+	BIOME_FLOWER_FOREST = "minecraft:flower_forest"
+
+	// Birch forest
+	BIOME_BIRCH_FOREST            = "minecraft:birch_forest"
+	BIOME_OLD_GROWTH_BIRCH_FOREST = "minecraft:old_growth_birch_forest"
+
+	// Dark forest
+	BIOME_DARK_FOREST = "minecraft:dark_forest"
+
+	// Taiga
+	BIOME_TAIGA                   = "minecraft:taiga"
+	BIOME_OLD_GROWTH_PINE_TAIGA   = "minecraft:old_growth_pine_taiga"
+	BIOME_OLD_GROWTH_SPRUCE_TAIGA = "minecraft:old_growth_spruce_taiga"
+
+	//Windswept
+	BIOME_WINDSWEPT_HILLS          = "minecraft:windswept_hills"
+	BIOME_WINDSWEPT_FOREST         = "minecraft:windswept_forest"
+	BIOME_WINDSWEPT_GRAVELLY_HILLS = "minecraft:windswept_gravelly_hills"
+
+	BIOME_CHERRY_GROVE = "minecraft:cherry_grove"
+	BIOME_PALE_GARDEN  = "minecraft:pale_garden"
+
+	//Cold ones
+	BIOME_SNOWY_PLAINS = "minecraft:snowy_plains"
+	BIOME_ICE_SPIKES   = "minecraft:ice_spikes"
+	BIOME_SNOWY_TAIGA  = "minecraft:snowy_taiga"
+	BIOME_SNOWY_BEACH  = "minecraft:snowy_beach"
+	BIOME_SNOWY_SLOPES = "minecraft:snowy_slopes"
+	BIOME_FROZEN_PEAKS = "minecraft:frozen_peaks"
+	BIOME_JAGGED_PEAKS = "minecraft:jagged_peaks"
+	BIOME_GROVE        = "minecraft:grove"
+
+	//Swamp
+	BIOME_SWAMP          = "minecraft:swamp"
+	BIOME_MANGROVE_SWAMP = "minecraft:mangrove_swamp"
+
+	//Jungle
+	BIOME_JUNGLE        = "minecraft:jungle"
+	BIOME_BAMBOO_JUNGLE = "minecraft:bamboo_jungle"
+	BIOME_SPARSE_JUNGLE = "minecraft:sparse_jungle"
+
+	//Beaches
+	BIOME_BEACH       = "minecraft:beach"
+	BIOME_STONE_SHORE = "minecraft:stone_shore"
+
+	//Rivers
+	BIOME_RIVER        = "minecraft:river"
+	BIOME_FROZEN_RIVER = "minecraft:frozen_river"
+
+	//Oceans
+	BIOME_OCEAN             = "minecraft:ocean"
+	BIOME_WARM_OCEAN        = "minecraft:warm_ocean"
+	BIOME_LUKEWARM_OCEAN    = "minecraft:lukewarm_ocean"
+	BIOME_COLD_OCEAN        = "minecraft:cold_ocean"
+	BIOME_FROZEN_OCEAN      = "minecraft:frozen_ocean"
+	BIOME_DEEP_FROZEN_OCEAN = "minecraft:deep_frozen_ocean"
+
+	//Caves
+	BIOME_LUSH_CAVES      = "minecraft:lush_caves"
+	BIOME_DRIPSTONE_CAVES = "minecraft:dripstone_caves"
+	BIOME_DEEP_DARK       = "minecraft:deep_dark"
+)
+
+// Nether biomes
+const (
+	BIOME_NETHER_WASTES    = "minecraft:nether_wastes"
+	BIOME_SOUL_SAND_VALLEY = "minecraft:soul_sand_valley"
+	BIOME_CRIMSON_FOREST   = "minecraft:crimson_forest"
+	BIOME_WARPED_FOREST    = "minecraft:warped_forest"
+	BIOME_BASALT_DELTAS    = "minecraft:basalt_deltas"
+)
+
+// End biomes
+const (
+	BIOME_THE_END           = "minecraft:the_end"
+	BIOME_SMALL_END_ISLANDS = "minecraft:small_end_islands"
+	BIOME_END_MIDLANDS      = "minecraft:end_midlands"
+	BIOME_END_HIGHLANDS     = "minecraft:end_highlands"
+	BIOME_END_BARRENS       = "minecraft:end_barrens"
+)
