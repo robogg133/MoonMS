d57dd25d9a8341e17be98025eeb84f16eb45ac8b
robogg133 <Luizfernandojoaquimdasilva@proton.me>
2026-03-15 15:01:44 -0300

    feat: translation codes

---
+++ internal/shared/language_codes.go
@@ -0,0 +1,8 @@
+package shared
+
+const (
+	MULTIPLAYER_DISCONNECT_IP_BANNED           = "multiplayer.disconnect.ip_banned"          // "You have been IP banned from this server"
+	MULTIPLAYER_DISCONNECT_BANNED              = "multiplayer.disconnect.banned"             // "You are banned from this server"
+	MULTIPLAYER_DISCONNECT_NOT_WHITELISTED     = "multiplayer.disconnect.not_whitelisted"    //  "You are not white-listed on this server!"
+	DISCONNECT_LOGINFAILEDINFO_INVALID_SESSION = "disconnect.loginFailedInfo.invalidSession" // "Invalid session (Try restarting your game and the launcher)"
+)
