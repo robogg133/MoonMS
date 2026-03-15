d35b35dc0bb435244718e918642c1d300750d4e8
robogg133 <Luizfernandojoaquimdasilva@proton.me>
2026-03-15 15:03:58 -0300

    feat: using plugin logger and check if called function exists

--- plugin/wasm/runtime.go
+++ plugin/wasm/runtime.go
@@ -2,11 +2,14 @@ package wasm
 
 import (
 	"context"
+	"fmt"
 	"io"
+	"io/fs"
 	"time"
 
 	"github.com/tetratelabs/wazero"
 	"github.com/tetratelabs/wazero/api"
+	"github.com/tetratelabs/wazero/imports/wasi_snapshot_preview1"
 )
 
 type Runtime struct {
@@ -21,27 +24,33 @@ type Runtime struct {
 	mod       api.Module
 	state     wazero.Runtime
 
+	logwriter io.Writer
+
 	file []byte
 }
 
-func NewRuntime(w io.Writer, file []byte) Runtime {
+func NewRuntime(w io.Writer, file []byte, fs fs.FS) *Runtime {
 
-	var r Runtime
+	r := &Runtime{
+		logwriter: w,
+	}
 
 	r.moduleCfg = wazero.NewModuleConfig().
 		WithName("plugin").
-		WithStdout(w).
-		WithStderr(w).
-		WithStdin(nil)
+		WithStdout(r.logwriter).
+		WithStderr(r.logwriter).
+		WithStdin(nil).
+		WithFS(fs)
 
 	r.ctx, r.cancel = context.WithCancel(context.Background())
 	r.state = wazero.NewRuntime(r.ctx)
+	wasi_snapshot_preview1.MustInstantiate(r.ctx, r.state)
 	r.file = file
+
 	return r
 }
 
 func (r *Runtime) Load() error {
-
 	deadline, cancel := context.WithDeadline(r.ctx, time.Now().Add(30*time.Second))
 	defer cancel()
 
@@ -58,7 +67,7 @@ func (r *Runtime) Load() error {
 	}
 
 	r.alloc = newBasicAlloc(r.mod.Memory(), r.mod.ExportedFunction("alloc"), &r.ctx)
-
+	r.enabled = true
 	return nil
 }
 
@@ -86,6 +95,9 @@ func (r *Runtime) Call(name string, params ...any) error {
 	}
 
 	fn := r.mod.ExportedFunction(name)
+	if fn == nil {
+		return fmt.Errorf("unkown function %s", name)
+	}
 
 	mParams := marshalParams(r.alloc, params...)
 
@@ -97,7 +109,7 @@ func (r *Runtime) Call(name string, params ...any) error {
 	return err
 }
 
-func (r *Runtime) Stop() error {
+func (r *Runtime) Close() error {
 	r.cancel()
 	err := r.state.Close(r.ctx)
 	return err
