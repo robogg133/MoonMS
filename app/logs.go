2a39dde2afdfa565f7adf81055ead0ba30ceeb10
robogg133 <Luizfernandojoaquimdasilva@proton.me>
2026-06-23 11:02:52 -0300

    ref: better debug logs, adding function caller and sourcefile

--- app/logs.go
+++ app/logs.go
@@ -8,6 +8,7 @@ import (
 	"path/filepath"
 	"strings"
 	"time"
+	"runtime"
 
 	"github.com/robogg133/MoonMS/internal/shared/plataforms"
 )
@@ -36,8 +37,9 @@ func (pl *PluginWriter) Write(b []byte) (int, error) {
 func (s *Server) LogInfo(format string, args ...any) {
 	timestamp := time.Now().Format("2006/01/02 15:04:05")
 	message := fmt.Sprintf(format, args...)
-	fmt.Printf("[%s] \033[34m[INFO]:\033[0m %v\n", timestamp, message)
-	fmt.Fprintf(s.logFile, "[%s] [INFO]: %v\n", timestamp, message)
+	debug := s.debugStatus()+":"
+	fmt.Printf("[%s] \033[34m[INFO]\033[0m%s %v\n", timestamp, addDebugColor(debug), message)
+	fmt.Fprintf(s.logFile, "[%s] [INFO]%s %v\n", timestamp, debug, message)
 }
 
 func (s *Server) LogDebug(format string, args ...any) {
@@ -46,36 +48,40 @@ func (s *Server) LogDebug(format string, args ...any) {
 	}
 	timestamp := time.Now().Format("2006/01/02 15:04:05")
 	message := fmt.Sprintf(format, args...)
-	fmt.Printf("[%s] \033[33m[DEBUG]:\033[0m %v\n", timestamp, message)
-	fmt.Fprintf(s.logFile, "[%s] [DEBUG]: %v\n", timestamp, message)
+	debug := s.debugStatus()+":"
+	fmt.Printf("[%s] \033[33m[DEBUG]%s\033[0m %v\n", timestamp, debug, message)
+	fmt.Fprintf(s.logFile, "[%s] [DEBUG]%s %v\n", timestamp, debug, message)
 }
 
 func (s *Server) LogWarn(format string, args ...any) {
 	timestamp := time.Now().Format("2006/01/02 15:04:05")
 	message := fmt.Sprintf(format, args...)
-	fmt.Printf("[%s] \033[33m[WARN]:\033[0m %v\n", timestamp, message)
-	fmt.Fprintf(s.logFile, "[%s] [WARN]: %v\n", timestamp, message)
+	debug := s.debugStatus()+":"
+	fmt.Printf("[%s] \033[33m[WARN]\033[0m%s %v\n", timestamp,addDebugColor(debug), message)
+	fmt.Fprintf(s.logFile, "[%s] [WARN]%s %v\n", timestamp, debug, message)
 }
 
 func (s *Server) LogError(format string, args ...any) {
 	timestamp := time.Now().Format("2006/01/02 15:04:05")
 	message := fmt.Sprintf(format, args...)
-	fmt.Printf("[%s] \033[31m[ERROR]:\033[0m %v\n", timestamp, message)
-	fmt.Fprintf(s.logFile, "[%s] [ERROR]: %v\n", timestamp, message)
+	debug := s.debugStatus()+":"
+	fmt.Printf("[%s] \033[31m[ERROR]\033[0m%s %v\n", timestamp, addDebugColor(debug), message)
+	fmt.Fprintf(s.logFile, "[%s] [ERROR]%s %v\n", timestamp, debug, message)
 }
 
 func (s *Server) LogPanic(format string, args ...any) {
 	timestamp := time.Now().Format("2006/01/02 15:04:05")
 	message := fmt.Sprintf(format, args...)
-	fmt.Printf("[%s] \033[31m[PANIC]:\033[0m %v\n", timestamp, message)
-	fmt.Fprintf(s.logFile, "[%s] [PANIC]: %v\n", timestamp, message)
+	debug := s.debugStatus()+":"
+	fmt.Printf("[%s] \033[31m[PANIC]:\033[0m%s %v\n", timestamp, addDebugColor(debug), message)
+	fmt.Fprintf(s.logFile, "[%s] [PANIC]%s %v\n", timestamp, debug, message)
 }
 
 func (s *Server) LogPlugin(pluginName string, args ...any) {
 	timestamp := time.Now().Format("2006/01/02 15:04:05")
 	message := fmt.Sprintf("%v", args...)
-	fmt.Printf("[%s] \033[32m[%s]:\033[0m %v \n", timestamp, pluginName, message)
-	fmt.Fprintf(s.logFile, "[%s] [%s]: %v\n", timestamp, pluginName, message)
+	fmt.Printf("[%s] \033[32m[%s]\033[0m %v \n", timestamp, pluginName, message)
+	fmt.Fprintf(s.logFile, "[%s] [%s] %v\n", timestamp, pluginName, message)
 }
 
 func (s *Server) StartLogger() error {
@@ -157,4 +163,21 @@ func (l *logWrapper) Warningf(s string, args ...interface{}) {
 
 func (l *logWrapper) Infof(s string, args ...interface{}) {
 	l.server.LogDebug(s, args...)
+}
+
+func addDebugColor(s string) string {
+	if s == "" || s == ":" {
+		return s
+	}
+
+	return "\033[33m"+s+"\033[0m"
+}
+
+func (s *Server) debugStatus() string {
+	if !s.Config.DebugEnabled {
+		return ""
+	}
+	pc, filename, line, _ := runtime.Caller(2)
+	
+	return fmt.Sprintf(" [%s] [%s:%d]", runtime.FuncForPC(pc).Name(), filename, line)	
 }
